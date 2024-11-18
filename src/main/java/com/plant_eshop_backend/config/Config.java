4a5140f28a573d5d83881321e23b5fda8604b9bd
devnsk <devnsk.2024@gmail.com>
2024-11-18 07:33:15 +0530

    JavaMailSender bean configured

--- src/main/java/com/plant_eshop_backend/config/Config.java
+++ src/main/java/com/plant_eshop_backend/config/Config.java
@@ -3,10 +3,36 @@ package com.plant_eshop_backend.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 public class Config {
+    @Value("${spring.mail.host}")
+    private String mailHost;
+    @Value("${spring.mail.port}")
+    private String mailPort;
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(mailHost);
+        javaMailSender.setPort(Integer.parseInt(mailPort));
+        javaMailSender.setUsername(mailUsername);
+        javaMailSender.setPassword(mailPassword);
+
+        Properties props = javaMailSender.getJavaMailProperties();
+        props.put("mail.smtp.starttls.enable", "true");
+        return javaMailSender;
+    }
     @Bean
     public ModelMapper mapper(){
         return new ModelMapper();
@@ -19,5 +45,6 @@ public class Config {
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
+
 
 }
