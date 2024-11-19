fa913a69597d77434c4afdd732d1afe31c8ee234
Sudipt Kumar Moharana <104424491+moharanasudipt@users.noreply.github.com>
2024-11-19 19:29:56 +0530

    Order Card Entity

---
+++ src/main/java/com/plant_eshop_backend/entity/Cart.java
@@ -0,0 +1,22 @@
+package com.plant_eshop_backend.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private int quantity;
+
+}
